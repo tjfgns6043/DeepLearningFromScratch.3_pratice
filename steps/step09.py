405b55130c85faa9c5ee858461e2ac3e8889db6c
Seol <hello.git.tjfgns6043@gmail.com>
2024-01-25 22:49:46 +0900

    day3 study 역전파 자동화와 테스트하는 코드를 추가했습니다.

---
+++ steps/step09.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+class Variable():
+  def __init__(self, data):
+    if data is not None:
+      if not isinstance(data, np.ndarray):
+        raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+    self.data = data
+    self.grad = None
+    self.creator = None
+    
+  def set_creator(self, func):
+    self.creator = func
+    
+  def backward(self):
+    if self.grad is None:
+      self.grad = np.ones_like(self.data)
+    
+    funcs = [self.creator]
+    while funcs:
+      f = funcs.pop() #함수를 가져온다.
+      x, y = f.input, f.output #함수의 입력과 출력을 가져온다.
+      x.grad = f.backward(y.grad) #backward 메서드를 호출한다.
+      
+      if x.creator is not None:
+        funcs.append(x.creator) # 하나 앞의 함수를 리스트에 추가한다.
+        
+class Function():
+  def __call__(self, input):
+    x = input.data
+    y = self.forward(x)
+    output = Variable(as_array(y))
+    output.set_creator(self)  # 출력 변수에 창조자를 설정한다.
+    self.input = input
+    self.output = output # 출력도 저장한다.
+    return output
+  def forward(self, x):
+    raise NotImplementedError()
+  
+  def backward(self, gy):
+    raise NotImplementedError()
+
+
+class Square(Function):
+  def forward(self, x):
+    y = x ** 2
+    return y
+  
+  def backward(self, gy):
+    x = self.input.data
+    gx = 2 * x * gy
+    return gx
+  
+class Exp(Function):
+  def forward(self, x):
+    y = np.exp(x)
+    return y
+  
+  def backward(self, gy):
+    x = self.input.data
+    gx = np.exp(x) * gy
+    return gx
+
+
+def square(x):
+  f = Square()
+  return f(x)
+
+def exp(x):
+  f = Exp()
+  return f(x)
+
+def as_array(x):
+  if np.isscalar(x):
+    return np.array(x)
+  return x
+
+
+print(np.isscalar(np.float64(1.0)))
+x = Variable(np.array(0.5))
+y = square(exp(square(x)))
+# a = square(x)
+# b = exp(a)
+# y = square(b)
+
+# 역전파
+# y.grad = np.array(1.0)
+y.backward()
+print(x.grad)
