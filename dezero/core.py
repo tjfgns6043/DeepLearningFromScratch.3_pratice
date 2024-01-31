df1c3ad28e1d7796d01eee5debfea47b6101a724
seolhun <175091@jnu.ac.kr>
2024-01-31 21:49:51 +0900

    day9: DeZero를 패키지화 하여 사용하기 편리하게 개선하였고, 연산자 오버로딩을 통해서 복잡한 수식에도 안정적인 미분을 수행할 수 있게 되었습니다.

--- dezero/core.py
+++ dezero/core.py
@@ -1,6 +1,10 @@
-import numpy as np
 import weakref
+import numpy as np
 import contextlib
+
+
+class Config:
+   enable_backprop = True
 
 class Variable:
     __array_priority__ = 200
@@ -80,13 +84,6 @@ class Variable:
                for y in f.outputs:
                   y().grad = None # y는 약한 참조
 
-
-def as_array(x):
-    if np.isscalar(x):
-        return np.array(x)
-    return x
-
-
 class Function:
     def __call__(self, *inputs):
         inputs = [as_variable(x) for x in inputs]
@@ -110,21 +107,6 @@ class Function:
     def backward(self, gys):
         raise NotImplementedError()
 
-
-class Square(Function):
-    def forward(self, x):
-        y = x ** 2
-        return y
-
-    def backward(self, gy):
-        x = self.inputs[0].data # 수정 전:  x = self.input.data
-        gx = 2 * x * gy
-        return gx
-
-
-def square(x):
-    return Square()(x)
-
 class Add(Function):
   def forward(self, x0, x1):
     y = x0 + x1
@@ -132,26 +114,7 @@ class Add(Function):
   
   def backward(self, gy):
     return gy, gy
-
-def add(x0, x1):
-  x1 = as_array(x1)
-  return Add()(x0, x1) 
-
-class Config:
-   enable_backprop = True
-
-@contextlib.contextmanager
-def using_config(name, value):
-    old_value = getattr(Config, name)
-    setattr(Config, name, value)
-    try:
-       yield
-    finally:
-       setattr(Config, name, old_value)
-
-def no_grad():
-   return using_config('enable_backprop', False)
-
+  
 class Mul(Function):
    def forward(self, x0, x1):
       y = x0 * x1
@@ -161,20 +124,6 @@ class Mul(Function):
       x0, x1 = self.inputs[0].data, self.inputs[1].data
       return gy * x1, gy * x0
 
-def mul(x0, x1):
-   x1 = as_array(x1)
-   return Mul()(x0, x1)
-
-Variable.__mul__ = mul
-Variable.__rmul__ = mul
-Variable.__add__ = add
-Variable.__radd__ = add
-
-def as_variable(obj):
-  if isinstance(obj, Variable):
-    return obj
-  return Variable(obj)
-
 class Neg(Function):
   def forward(self, x):
     return -x
@@ -182,27 +131,13 @@ class Neg(Function):
   def backward(self, gy):
     return -gy
 
-def neg(x):
-  return Neg()(x)
-
-Variable.__neg__ = neg
-
 class Sub(Function):
   def forward(self, x0, x1):
     y = x0 - x1
     return y
   def backward(self, gy):
     return gy, -gy
-def sub(x0, x1):
-  x1 = as_array(x1)
-  return Sub()(x0, x1)
-Variable.__sub__ = sub
-
-def rsub(x0, x1):
-  x1 = as_array(x1)
-  return Sub(x1, x0)
-Variable.__rsub__ = rsub
-
+  
 class Div(Function):
   def forward(self, x0, x1):
     y = x0 / x1
@@ -212,16 +147,6 @@ class Div(Function):
     gx0 = gy / x1
     gx1 = gy * (-x0 / x1 ** 2)
     return gx0, gx1
-  
-def div(x0, x1):
-  x1 = as_array(x1)
-  return Div()(x0, x1)
-
-def rdiv(x0, x1):
-  x1 = as_array(x1)
-  return Div()(x1, x0) 
-Variable.__truediv__ = div
-Variable.__rtruediv__ = rdiv
 
 class Pow(Function):
   def __init__(self, c):
@@ -233,19 +158,68 @@ class Pow(Function):
     c = self.c
     gx = c * x ** (c - 1) * gy
     return gx
+  
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+       yield
+    finally:
+       setattr(Config, name, old_value)
+
+def no_grad():
+   return using_config('enable_backprop', False)
+ 
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+  
+def as_variable(obj):
+  if isinstance(obj, Variable):
+    return obj
+  return Variable(obj)
+
+def add(x0, x1):
+  x1 = as_array(x1)
+  return Add()(x0, x1) 
+
+def mul(x0, x1):
+   x1 = as_array(x1)
+   return Mul()(x0, x1)
+
+def neg(x):
+  return Neg()(x)
+
+def sub(x0, x1):
+  x1 = as_array(x1)
+  return Sub()(x0, x1)
+
+def rsub(x0, x1):
+  x1 = as_array(x1)
+  return Sub(x1, x0)
+
+def div(x0, x1):
+  x1 = as_array(x1)
+  return Div()(x0, x1)
+
+def rdiv(x0, x1):
+  x1 = as_array(x1)
+  return Div()(x1, x0)
 
 def pow(x, c):
   return Pow(c)(x)
 
-Variable.__pow__ = pow
+def setup_variable():
+  Variable.__add__ = add
+  Variable.__radd__ = add
+  Variable.__mul__ = mul
+  Variable.__rmul__ = mul
+  Variable.__neg__ = neg
+  Variable.__sub__ = sub
+  Variable.__rsub__ = rsub
+  Variable.__truediv__ = div
+  Variable.__rturediv__ = rdiv
+  Variable.__pow__ = pow
   
-x = Variable(np.array(2.0))
-y = 3.0 * x + 1.0
-print(y)
-
-x = Variable(np.array([1.0]))
-y = np.array([2.0]) + x
-
-x = Variable(np.array(2.0))
-print(x ** 3)
-
