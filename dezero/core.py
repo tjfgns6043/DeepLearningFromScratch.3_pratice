a62be54ef361741b808037244cdb3e3db32492af
seolhun <175091@jnu.ac.kr>
2024-02-21 02:21:03 +0900

    day14: 뉴턴 방법으로 푸는 최적화

--- dezero/core.py
+++ dezero/core.py
@@ -161,7 +161,7 @@ class Pow(Function):
   def forward(self, x):
     return x ** self.c
   def backward(self, gy):
-    x = self.inputs
+    x = self.inputs[0]
     c = self.c
     gx = c * x ** (c - 1) * gy
     return gx
